d2f217586489e0ecaa8519effe2f7bea73e04220
masum <masum.talukder@appinionbd.com>
2022-07-17 14:39:47 +0600

    open close principle

--- src/solid_principle/open_close_principle/OpenClosePrinciple.java
+++ src/solid_principle/open_close_principle/OpenClosePrinciple.java
@@ -4,8 +4,10 @@ public class OpenClosePrinciple {
 
     public static void main(String[] args) {
         Addition addition = new Addition(10.2, 30);
+        Subtraction subtraction = new Subtraction(10.2, 30);
 
         Calculator calculator = new Calculator();
         calculator.Calculate(addition);
+        calculator.Calculate(subtraction);
     }
 }
